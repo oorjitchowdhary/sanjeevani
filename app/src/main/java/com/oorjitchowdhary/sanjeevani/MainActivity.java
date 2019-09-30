c7e6825262d777f54e7666e2b0382ab529e3ba1b
Laxya Pahuja <laxya.pahuja8@gmail.com>
2019-09-30 22:44:36 +0530

    ui

--- app/src/main/java/com/oorjitchowdhary/sanjeevani/MainActivity.java
+++ app/src/main/java/com/oorjitchowdhary/sanjeevani/MainActivity.java
@@ -1,9 +1,11 @@
 package com.oorjitchowdhary.sanjeevani;
 
 import android.content.Intent;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
